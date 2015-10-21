1a10aa71fbe38d374f150ce1ef549d30c4615c56
Juniper Chapman <juniper@wl-84-16.wireless.umt.edu>
2015-10-21 09:20:57 -0600

    small app.js edits, preparing to add react

--- public/javascript/app.js
+++ public/javascript/app.js
@@ -1,12 +1,12 @@
 (function(){
-$.getJSON( "http://localhost:3000/api/blogs", function(data) {
-  var items = [];
-  $.each( data, function(key, val) {
-    items.push("<div class='post'><div class='entry-meta'><div class='date'>" + val.date + "</div></div><div class='main'><h2 class='entry-title'>" + val.title +"</h2><div class='entry-content'>" + val.content + "</div></div></div>");
-  });
-  $("<div/>",{
-    "class": "blogs-area",
-    html: items.join("")
-  }).appendTo("#blog-posts");
-});
+	$.getJSON( "http://localhost:3000/api/blogs", function(data) {
+		var items = [];
+		$.each( data, function(key, val) {
+			items.push("<div class='post'><div class='entry-meta'><div class='date'>" + val.date + "</div></div><div class='main'><h2 class='entry-title'>" + val.title +"</h2><div class='entry-content'>" + val.content + "</div></div></div>");
+		});
+		$("<div/>",{
+			"class": "blogs-area",
+			html: items.join("")
+		}).appendTo("#blog-posts");
+	});
 })();
