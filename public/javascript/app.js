66efe2725aa194b05041a034b6fe7c2cf109404d
Juniper Chapman <juniper@wl-80-93.wireless.umt.edu>
2015-10-21 15:14:58 -0600

    things work again

--- public/javascript/app.js
+++ public/javascript/app.js
@@ -1,12 +1,52 @@
-(function(){
-	$.getJSON( "http://localhost:3000/api/blogs", function(data) {
-		var items = [];
-		$.each( data, function(key, val) {
-			items.push("<div class='post'><div class='entry-meta'><div class='date'>" + val.date + "</div></div><div class='main'><h2 class='entry-title'>" + val.title +"</h2><div class='entry-content'>" + val.content + "</div></div></div>");
+var BlogList = React.createClass({
+	render: function() {
+		var oneBlog = this.props.data.map(function(blog){
+			return 	(<div>
+					<div className='entry-meta'><div className='date'> {blog.date} </div></div>
+					<div className='main'><h2 className='entry-title'> {blog.title} </h2>
+					<div className='entry-content'> {blog.content} </div></div>
+					</div>)
 		});
-		$("<div/>",{
-			"class": "blogs-area",
-			html: items.join("")
-		}).appendTo("#blog-posts");
-	});
-})();
+		return (
+			<div className='post'>
+				{oneBlog}
+			</div>
+			);
+	}
+});
+
+var BlogBox = React.createClass({
+	//Set initial state-----------------
+	getInitialState: function(){
+		return{data: []};
+	},
+	//Fetch data from our server--------------
+	loadBlogsFromServer: function(){
+		$.ajax({
+			url:this.props.url,
+			dataType:"json",
+			cache: false,
+			success: function(data){
+				console.log("inside success")
+				this.setState({data: data});
+			}.bind(this),
+			error: function(xhr, status, err){
+				console.log("broken url is " + this.props.url)
+				console.error(this.props.url, status, err.toString());
+			}.bind(this)
+		});
+	},
+	//Mount components------------------
+	componentDidMount: function(){
+		this.loadBlogsFromServer();
+	},
+	render: function() {
+		return (
+			<div id="blog-posts" className="clearfix blogs-area">
+				<BlogList data={this.state.data}/>
+			</div>
+		);
+	}
+});
+
+React.render(<BlogBox url="/api/blogs/"/>, document.getElementById("blog-area"));
