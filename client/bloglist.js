437ad83ac6d29d9375ebc3a4afb124c6fc5ce6b1
Juniper Chapman <junipie@gmail.com>
2015-11-24 16:58:43 -0700

    Gravatars populate in blog comments, comment format adjusted to look nice with images.

--- client/bloglist.js
+++ client/bloglist.js
@@ -1,4 +1,5 @@
 var React = require('react');
+var md5 = require('md5');
 var CommentForm = require('./commentForm');
 
 var BlogList = React.createClass({
@@ -12,19 +13,37 @@ var BlogList = React.createClass({
 
 			var commentList = blog.comments.map(function(c){
 				if(c.user){
-		          var user = c.user.local.email;
+		            var user = c.user.local.email;
+		            var GRAVATAR_URL = "http://gravatar.com/avatar";
+		        	var hash = md5(user);
+	 				var size = 60;
+	 				var url = GRAVATAR_URL + "/" + hash + "?s=" + size;
 		        } else {
-		          var user = "anonymous"
+		          	var user = "anonymous";
+		          	var url = "./img/blank-gravatar.png";
 		        }
 				var originDate = c.date;
 				var commentDate = originDate.substring(0, 10);
 				return (
 					<div>
 						<div className='row'>
-							<h5 className='col-xs-8'>Posted by: {user}</h5>
-							<p className='col-xs-4'><i>{commentDate}</i></p>
-						</div>
-							<p>{c.body}</p>
+							<div className = "col-sm-2">
+						      <div className = "thumbnail">
+						         <img src = {url}/>
+						      </div>
+						    </div>
+						    <div className="col-sm-10">
+						    	<div className="row">
+								    <div className="col-sm-8">
+								         <h5>Posted by: {user}</h5>						         
+								    </div>
+									<p className='col-xs-4'><i>{commentDate}</i></p>
+								</div>
+								<div className="row">
+									<p>{c.body}</p>
+								</div>
+							</div>
+						</div><br/>
 					</div>
 					)
 			})
